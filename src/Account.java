b5156753396664cd5a7c516a100d24106382184e
Aliki Orlandou <73021053+aorlandou@users.noreply.github.com>
2020-11-14 14:45:22 +0200

    Update Account.java

--- src/Account.java
+++ src/Account.java
@@ -2,8 +2,8 @@
 public class Account {
 	private  String name, password, phone, birthdate, gender, municipality;
 	private boolean covid;
-  
-  void userState (boolean x){
+  	
+	void userState (boolean x){
 	  covid=x ;
 	  sendNotification(covid) ;
 	}
