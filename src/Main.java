ae368f6f25eb992585c5828c5fd1b5bd2ed9cfd3
AlexandriEleni <eleni.alexandri@yahoo.gr>
2020-11-14 21:17:33 +0200

    change

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.InputMismatchException;
 public class Main {
-	public static void main(String[] args){
+	public static void main(String[] args)throws InputMismatchException {
 		int choice;
 		Scanner sc = new Scanner(System.in);
 		do{
