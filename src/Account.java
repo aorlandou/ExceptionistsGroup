d25b06f06661b265e218e1df31b64711ac0f1f88
aorlandou <73021053+aorlandou@users.noreply.github.com>
2020-11-15 22:58:55 +0200

    creation of database
    tables account friends and notification were created

--- src/Account.java
+++ src/Account.java
@@ -3,7 +3,7 @@ public class Account {
 	private  String name, password, phone, birthdate, gender, municipality;
 	private boolean covid;
   
-  void userState (boolean x){
+	void userState (boolean x){
 	  covid=x ;
 	  sendNotification(covid) ;
 	}
