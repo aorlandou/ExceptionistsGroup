4a61cd6be0366f0359ab2313a4c7ac36ae069229
constantinazouni <72806587+constantinazouni@users.noreply.github.com>
2020-11-16 11:16:47 +0200

    Unfinished Account.java

--- src/Account.java
+++ src/Account.java
@@ -1,6 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.text.SimpleDateFormat;  
+import java.util.Date;  
 public class Account {
-	private  String name, password, phone, birthdate, gender, municipality;
+	private  String name,surname, password, phone, birthdate, gender, municipality;
 	private boolean covid;
+	private String message;
+	List<String> notifications= new ArrayList<String>();
+	List<Account> friends= new ArrayList<Account>();
 	
+	public void sendNotification() {
+		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");  
+	    Date date = new Date();  
+		if(covid==true) {
+			message="Your friend "+surname+" "+name+" has been tested possitive for coronavirus "+formatter.format(date);
+		}else {
+			message="Your friend "+surname+" "+name+" has recovered from coronavirus "+formatter.format(date);
+		}
+	}
 	
 }
