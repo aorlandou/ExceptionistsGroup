7fd73d35a12a784e5c6b30c88b30d96bec289b0b
aorlandou <73021053+aorlandou@users.noreply.github.com>
2020-11-18 21:29:37 +0200

    database connection

--- src/Account.java
+++ src/Account.java
@@ -1,10 +1,24 @@
 
 public class Account {
-	private  String name, password, phone, birthdate, gender, municipality;
+	private  String name, surname, password, phone, birthdate, gender, municipality;
 	private boolean covid;
 
 	void userState (boolean x){
 	  covid=x ;
 	  sendNotification(covid) ;
 	}
+
+	public Account(String name, String surname, String password, String phone, String birthdate, String gender, String municipality,
+			boolean covid) {
+		super();
+		this.name = name;
+		this.password = password;
+		this.phone = phone;
+		this.birthdate = birthdate;
+		this.gender = gender;
+		this.municipality = municipality;
+		this.covid = covid;
+	}
+	
+
 }
