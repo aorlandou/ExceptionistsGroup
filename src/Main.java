ed8f40491603d59fa4768488c224f6c8cb36e111
AlexandriEleni <eleni.alexandri@yahoo.gr>
2020-11-23 22:29:58 +0200

    commit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 import databaseConnection.DB;
+import testaki.testaki;
 
 import javax.swing.JComponent;
 import java.awt.BorderLayout;
@@ -21,26 +22,29 @@ import javax.swing.JButton;
 
 public class Main extends JFrame implements ActionListener {
 	
+	public static final int WIDTH = 700;
+	public static final int HEIGHT = 600;
 	
-		//Connecting to our database
-		//DB data = new DB();
-		//data.getConnection();
-		public Main( ) {
-		super( );
-		JFrame jf = new JFrame("Window");
-        JLabel label = new JLabel("If you want to sign in press 1, if you want to log in press 2");
-        jf.add(label);
-        jf.setBounds(0, 0, 700, 100);
-        jf.setLayout(new FlowLayout());
-        JButton button1 = new JButton("button 1");
-        JButton button2 = new JButton("button 2");
-   
-        button1.addActionListener(this);
-        add(button1);
-        button2.addActionListener(this);
-        add(button2);
-		
-	}
+	//Connecting to our database
+	DB data = new DB();
+	data.getConnection();
+	public Main( ) {
+	super( );
+	setSize(WIDTH, HEIGHT);
+	setTitle("Window");
+	setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+	setLayout(new FlowLayout());
+	JLabel label = new JLabel("If you want to sign in press 1, if you want to log in press 2");
+	add(label);
+	JButton button1 = new JButton("button 1");
+	button1.addActionListener(this);
+    add(button1);
+    JButton button2 = new JButton("button 2");
+    button2.addActionListener(this);
+    add(button2);
+    
+	
+}
 	public void actionPerformed(ActionEvent e) {
 		String buttonType = e.getActionCommand();
 		switch (buttonType) {
@@ -57,8 +61,8 @@ public class Main extends JFrame implements ActionListener {
 	public static void main(String[] args) {
 		DB data = new DB();
 		data.getConnection();
-		JFrame jf = new JFrame("Window");
-		jf.setVisible(true);
+		Main w = new Main();
+		w.setVisible(true);
 	}
 }
 
