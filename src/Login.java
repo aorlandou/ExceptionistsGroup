592f19d32398582c23a221bb73e4dcec2bc82158
ioanna chalari <ichalari1@gmail.com>
2020-11-26 20:49:55 +0200

    Changes in DB and Login

--- src/Login.java
+++ src/Login.java
@@ -1,79 +1,79 @@
-import java.sql.DriverManager;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.ResultSet;
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
 public class Login {
-	
-	public static void loginMethod() throws InputMismatchException {
-		 
-		int answer; //user's phonenumber is expected
-		int pass; //user's password is expected
-		
-		Scanner sc = new Scanner(System.in);
-	
-		DB dbConnection = new DB(); //connect with DataBase
 
-		dbConnection.getConnection();
+	public void passwordControl() throws InputMismatchException {
+		DB dbObject = new DB(); //create DB object
+		Scanner sc = new Scanner(System.in); //Scanner object sc
+		try {
+			System.out.println("Please enter your phonenumber");
+			String phone = sc.next();
+			String answer = dbObject.getPassword(phone);
+			
+				if (answer.equals("nothing returned")) {
+					System.out.println("Sorry but this phonenumber does not exists \n Back in menu");
+					return;
+				} else {
+					System.out.println("Please enter your password");
+					String password = sc.next(); //user's password is expected
+					
+					answer = dbObject.getPassword(phone); //valid password from database is returned
+					control(answer, password, phone); //call control method
+				}// end else/ Procedure of getting the password from the user
 
-		boolean continueLoop = true;
-		do {
-			try { //read the phonenumber and the password from user
-				System.out.println("Please enter your phonenumber");
-				answer = sc.nextInt(); // user enters a number
-				
-					try {
-						//research on database	
-						Statement st = dbConnection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE); 
-						ResultSet frs = st.executeQuery("select answer from accounts");
-						int x = frs.getInt(1);
-								
-						if (x == answer) {
-						//the phonenumber exists
-									
-							String ans;
-							int y;
-							int i;
-							do {
-								System.out.println("Please enter your password to continue");
-								pass = sc.nextInt();
-								i++;
-								ResultSet srs = st.executeQuery("select pass from accounts");
-								y = srs.getInt(4);
-								if (y == pass) {
-											
-									//CONTINUE PROCESS OF LOGIN
-											
-								} else {
-									System.out.println("Invalid password\n Do you want to try again?");
-										ans = sc.nextLine();
-								}
-						
-							} while((ans.equals("no") || y == pass) && i > 5);
-								
-						} else { //phonenumber does not exist on database
-							System.out.println("We are sorry this phonenumber does not exist");
-							System.out.println("If you want to register please select the option Signin");
-						}
-										
-						} catch(ClassNotFoundException cnfe){
-								cnfe.printStackTrace();
-						}catch (SQLException se){
-								se.printStackTrace();
-						}
+		} catch(InputMismatchException e) {
+			sc.nextInt();
+			System.err.printf("\n Exception: %s \n", e);
+			System.out.println("Invalid data \n Try again");
+		} //end catch
+	} //end of passwordControl method
 
-				continueLoop = false; //no exceptions where found
-			} //end try
-			catch (InputMismatchException e1){ //In case of invalid data
-				System.err.printf("\n exception: %s \n", e1);
-				sc.nextLine(); //discard input so that user can try again
-				System.out.println("Please enter valid characters (integers");
+	public void control(String ans, String password, String phone) {
+		Scanner sc = new Scanner(System.in); //Scanner object sc
+		String tryAgain; //user enters yes/no do that to enter again his password
+		int count = 0; 
+		if (! ans.equals(password)) { //password from database doesn't match with the password, which user has typed
+			System.out.println("Sorry but your password in not correct");
+			System.out.println("Do you want to try again? (yes/no)");
+			tryAgain = sc.next();
+			count++; //increase trials
+			
+			if (tryAgain.equals("no")){ //User doesn't want to try again
+				System.out.println("Back in menu");
 			}
 
-		} while (continueLoop);
-
+			while (tryAgain.equals("yes") && count < 5) {  //only 5 times user can try to type his password
+				password = sc.next(); //user enters again the password
+				if (! ans.equals(password)) { //not valid password
+					count++;
+					System.out.println("Sorry but your password in not correct");
+					System.out.println("Do you want to try again? (yes/no)");
+					tryAgain = sc.next();
+				} else { //Now user can login
+					createCurrentAccount(phone);
+					break;
+				}
+			} //end while
+		} else { //Now user can login
+			createCurrentAccount(phone);
+		}
+	} //end of control method
+	
+	//Method that creates the current account
+	public void createCurrentAccount(String phone) {
+		DB db = new DB(); //create DB object
+		String name = db.getName(phone);
+		String surname = db.getSurname(phone);
+		String password = db.getPassword(phone);
+		String birthday = db.getBirthdate(phone);
+		String gender = db.getGender(phone);
+		String municipality = db.getMunicipality(phone);
+		
+		//The constructor of class Account is called 
+		Account currentAccount = new Account(name, surname, password, phone, birthday, gender, municipality);
+		//Print main menu
 	}
+	
+	
 }
