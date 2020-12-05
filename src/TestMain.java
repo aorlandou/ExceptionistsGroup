0fe715c8cab826765e48ba9e46f9ea9f3686c2b7
aorlandou <73021053+aorlandou@users.noreply.github.com>
2020-12-05 13:10:23 +0200

    corrections in editProfile and TestMain

--- src/TestMain.java
+++ src/TestMain.java
@@ -21,9 +21,10 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		
-	    String phone="phone";
-	    String password="password";
-		System.out.println("****** PROFILE EDIT ******");
+		DB data = new DB();
+	    String phone = "6977362135";
+	    String password = "den";
+	    System.out.println("****** PROFILE EDIT ******");
 		System.out.println(" [1] Change name.");
 		System.out.println(" [2] Change surname.");
 		System.out.println(" [3] Change password.");
@@ -33,7 +34,7 @@ public class TestMain {
 		
 		Scanner scanner1= new Scanner(System.in);
 		int choice=scanner1.nextInt();
-		DB data=new DB();
+		
 		while(choice!=0) {
 			switch (choice) {
 				case 1: 
@@ -54,24 +55,53 @@ public class TestMain {
 					System.out.println("Please enter the new password. ");
 					Scanner scanner4= new Scanner(System.in);
 					String password1=scanner4.nextLine(); 
-					System.out.println("Please enter again the new password. ");
+					System.out.println("Please enter the new password again. ");
 					Scanner scanner5= new Scanner(System.in);
 					String password2=scanner5.nextLine();
 					if(password1.equals(password2)) {
 						password=password1;
+						data.updatePassword(phone, password);
 						System.out.println("Password has been changed successfully.");
-						data.updatePassword(phone, password);}
+					}else {
+						int count = 3;
+						boolean cor = false;
+						while (count > 0 && cor == false) {
+							System.out.println("Wrong password.");
+							System.out.println("You have " + count + " trial(s) left to enter your new "
+									+ "password correctly.");
+							System.out.println("Please try again and enter a new password.");
+							Scanner scanner6= new Scanner(System.in);
+							String pass1=scanner6.nextLine();
+							System.out.println("Please enter the new password again.");
+							String pass2=scanner5.nextLine();
+								if (pass1.equals(pass2)) {
+									password = pass1;
+									data.updatePassword(phone, password);
+									System.out.println("Password has been changed successfully.");
+									cor = true;
+								}else {
+									count--;
+								}
+						}
+						if (count == 0) {
+							System.out.println("You exceeded the number of trials you had to change "
+									+ "your password. \nYou are redirected back to PROFILE EDITING...");
+						}
+							
+					}
+					
 				break;
 				case 4:
 					System.out.println("Please enter the new municipality. ");
-					Scanner scanner6= new Scanner(System.in);
-					String municipality=scanner6.nextLine();
+					Scanner scanner7= new Scanner(System.in);
+					String municipality=scanner7.nextLine();
 					data.updateMunicipality(phone, municipality);
 					System.out.println("Municipality has been changed successfully.");
+				break;
 				case 5: 
 					 System.out.println("Do you want to delete your profile? If so, enter your password.");
-					 Scanner scanner7= new Scanner(System.in);
-					 String pass=scanner7.nextLine();
+					 Scanner scanner8= new Scanner(System.in);
+					 String pass=scanner8.nextLine();
 					 if(pass.equals(password)){
 						 try {
 							data.deleteAcc(phone);
@@ -83,6 +113,9 @@ public class TestMain {
 						 System.exit(0);
 						  } 
 				break;
+				case 0:
+					System.exit(0);
+				break;
 			}
 		
 			System.out.println("****** PROFILE EDIT ******");
@@ -93,8 +126,8 @@ public class TestMain {
 			System.out.println(" [5] Delete your profile.");
 			System.out.println(" [0] Finish profile editing.");
 			
-			Scanner scanner8= new Scanner(System.in);
-			choice =scanner8.nextInt();
+			Scanner scanner9= new Scanner(System.in);
+			choice =scanner9.nextInt();
 			}
 }
 }
