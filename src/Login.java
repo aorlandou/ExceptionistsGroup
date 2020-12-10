4a67abe90139a978359c90875418998a2291406f
ioanna chalari <ichalari1@gmail.com>
2020-12-10 19:11:50 +0200

    commit

--- src/Login.java
+++ src/Login.java
@@ -13,6 +13,7 @@ import java.awt.event.ActionListener;
 
 public class Login implements ActionListener  {
 
+<<<<<<< Updated upstream
 	private static JLabel phoneLabel;
 	private static JTextField phoneText;
 	private static JLabel passwordLabel;
@@ -22,6 +23,59 @@ public class Login implements ActionListener  {
 	private int count = 0; //counts how many trials user has done to login
 
 	public void loginmethod() { //method using GUI for the login procedure
+=======
+	public static void loginmethod() throws InputMismatchException {
+		DB dbObject = new DB(); //create DB object
+		Scanner sc = new Scanner(System.in); //Scanner object sc
+		try {
+			System.out.println("Please enter your phonenumber");
+			String phone = sc.next();
+			String answer = dbObject.getPassword(phone);
+			
+				if (answer.equals("nothing returned")) { //phone doesn't exist in the system
+					System.out.println("Sorry but this phonenumber does not exists \n Back in menu");
+					return; //User is returned in previous method
+				} else { //phone exists / continue process
+					boolean validpass; // validpass returns if user has entered a valid password
+					validpass = findPassword(answer); //call control method
+					if (validpass == true) { //user has typed valid password
+						System.out.println("You are connecting to your account");
+						createCurrentAccount(phone);
+						//go to main menu
+					} else { //user can not login to the system
+						System.out.println("Sorry you can not login \n Try again later");
+						//PROBLEM/system.exit? !!!
+					}
+				}// end else/ end procedure of login
+		} catch(InputMismatchException e) {
+			sc.nextInt();
+			System.err.printf("\n Exception: %s \n", e);
+			System.out.println("Invalid data \n Try again");
+		} //end catch
+		sc.close();
+	} //end of passwordControl method
+
+	public static boolean findPassword(String ans) {
+		Scanner sc = new Scanner(System.in); //Scanner object sc
+		String tryAgain = "yes"; //user enters yes/no do that to enter again his password
+		//We initialize the variable with "yes" so that the loop will be realized at least once
+		int count = 0; //count user's trials
+		String password; //user's password is expected
+		boolean found = false; //We examine if user has succeeded to login
+			while (tryAgain.equals("yes") && count < 5) {  //only 5 times user can try to type his password
+				System.out.println("Please enter your password");
+				password = sc.next(); //user enters again the password
+				count++;
+				if (! ans.equals(password)) { //not valid password
+					System.out.println("Sorry but your password in not correct");
+						if (count < 5) {
+						System.out.println("Do you want to try again? (yes/no). You have " + (5-count) + " tries left.");
+						tryAgain = sc.next();
+						} else {
+							System.out.println("No more trials");
+							break;
+						}//end else
+>>>>>>> Stashed changes
 
 		JFrame frame = new JFrame();
 		JPanel panel = new JPanel();
@@ -92,9 +146,15 @@ public class Login implements ActionListener  {
 		}
 	}
 
+<<<<<<< Updated upstream
 	// Method that creates the current account
 	public void createCurrentAccount(String phone) {
 		DB db = new DB(); // create DB object
+=======
+	//Method that creates the current account
+	public static void createCurrentAccount(String phone) {
+		DB db = new DB(); //create DB object
+>>>>>>> Stashed changes
 		String name = db.getName(phone);
 		String surname = db.getSurname(phone);
 		String password = db.getPassword(phone);
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import java.util.Scanner;
 import java.util.InputMismatchException;
 import javax.swing.JFrame;
@@ -54,3 +55,69 @@ public class Main extends JFrame implements ActionListener {
 		w.setVisible(true);
 	}
 }
+=======
+import java.util.Scanner;
+import java.util.InputMismatchException;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JComponent;
+import java.awt.BorderLayout;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Font;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+
+public class Main extends JFrame implements ActionListener {
+
+
+	public static final int WIDTH = 600;
+	public static final int HEIGHT = 200;
+
+	
+	//Connecting to our database
+	DB data = new DB();
+	data.getConnection();
+	public Main( ) {
+	super( );
+	setSize(WIDTH, HEIGHT);
+	setTitle("Window");
+	setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+	setLayout(new FlowLayout());
+	JLabel label = new JLabel("If you want to sign in press 1, if you want to log in press 2");
+	add(label);
+	JButton button1 = new JButton("button 1");
+	button1.addActionListener(this);
+    add(button1); 
+    JButton button2 = new JButton("button 2");
+    button2.addActionListener(this);
+    add(button2);
+    
+	
+}
+	public void actionPerformed(ActionEvent e) {
+		String buttonType = e.getActionCommand();
+		switch (buttonType) {
+		case "button 1":
+			SignUp.creatAccount();
+			break;
+		case "button 2":
+			Login.loginmethod();
+			break;
+		
+		}
+	}
+	public static void main(String[] args) {
+		DB data = new DB();
+		data.getConnection();
+		Main w = new Main();
+		w.setVisible(true);
+		
+	}
+}
+
+>>>>>>> Stashed changes
