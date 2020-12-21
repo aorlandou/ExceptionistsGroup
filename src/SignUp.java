158ca7ee95aa460d3860536fbb398140bb459200
AlexandriEleni <eleni.alexandri@yahoo.gr>
2020-12-21 23:15:59 +0200

    commit

--- src/SignUp.java
+++ src/SignUp.java
@@ -1,3 +1,4 @@
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
@@ -26,6 +27,7 @@ public class SignUp implements ActionListener{
 	private static JLabel municipalityLabel;
 	private static JTextField municipalityText;
 	private static JButton button;
+	private static JLabel success;
 	
 	public static void creatAccount() {
 		JFrame frame = new JFrame();
@@ -41,55 +43,66 @@ public class SignUp implements ActionListener{
 		nameLabel.setBounds(10, 20, 800, 25);
 		panel.add(nameLabel);
 		nameText = new JTextField();
-		nameText.setBounds(100, 20, 165, 25);
+		nameText.setBounds(150, 20, 165, 25);
 		panel.add(nameText);
 		
 		surnameLabel = new JLabel("Surname:");
 		surnameLabel.setBounds(10, 50, 800, 25);
 		panel.add(surnameLabel);
 		surnameText = new JTextField();
-		surnameText.setBounds(100, 50, 165, 25);
+		surnameText.setBounds(150, 50, 165, 25);
 		panel.add(surnameText);
 		
 		passwordLabel = new JLabel("Password:");
 		passwordLabel.setBounds(10, 80, 800, 25);
 		panel.add(passwordLabel);
 		passwordText = new JPasswordField();
-		passwordText.setBounds(100, 80, 165, 25);
+		passwordText.setBounds(150, 80, 165, 25);
 		panel.add(passwordText);
 		
+		password2Label = new JLabel("Confirm your password:");
+		password2Label.setBounds(10, 110, 800, 25);
+		panel.add(password2Label);
+		password2Text = new JPasswordField();
+		password2Text.setBounds(150, 110, 165, 25);
+		panel.add(password2Text);
+		
 		phoneLabel = new JLabel("Phone:");
-		phoneLabel.setBounds(10, 110, 800, 25);
+		phoneLabel.setBounds(10, 140, 800, 25);
 		panel.add(phoneLabel);
 		phoneText = new JTextField();
-		phoneText.setBounds(100, 110, 165, 25);
+		phoneText.setBounds(150, 140, 165, 25);
 		panel.add(phoneText);
 		
 		genderLabel = new JLabel("Gender:");
-		genderLabel.setBounds(10, 140, 800, 25);
+		genderLabel.setBounds(10, 170, 800, 25);
 		panel.add(genderLabel);
 		genderText = new JTextField();
-		genderText.setBounds(100, 140, 165, 25);
+		genderText.setBounds(150, 170, 165, 25);
 		panel.add(genderText);
 		
 		birthdateLabel = new JLabel("Birthdate:");
-		birthdateLabel.setBounds(10, 170, 800, 25);
+		birthdateLabel.setBounds(10, 200, 800, 25);
 		panel.add(birthdateLabel);
 		birthdateText = new JTextField();
-		birthdateText.setBounds(100, 170, 165, 25);
+		birthdateText.setBounds(150, 200, 165, 25);
 		panel.add(birthdateText);
 		
 		municipalityLabel = new JLabel("Municipality:");
-		municipalityLabel.setBounds(10, 200, 800, 25);
+		municipalityLabel.setBounds(10, 230, 800, 25);
 		panel.add(municipalityLabel);
 		municipalityText = new JTextField();
-		municipalityText.setBounds(100, 200, 165, 25);
+		municipalityText.setBounds(150, 230, 165, 25);
 		panel.add(municipalityText);
 		
 		button = new JButton("Sign up");
 		button.setBounds(10, 300 , 100, 65);
 		button.addActionListener(new SignUp());
 		panel.add(button);
+
+		success = new JLabel("");
+		success.setBounds(10, 110, 300, 25);
+		panel.add(success);
 
 		frame.setVisible(true);
 
@@ -99,33 +112,18 @@ public class SignUp implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
 		String name = nameText.getText();
 		String password = passwordText.getText();
+		String password2 = passwordText.getText();
 		String surname = surnameText.getText();
 		String phone = phoneText.getText();
 		String birthdate = birthdateText.getText();
 		String gender = genderText.getText();
 		String municipality = municipalityText.getText();
 		
-		JFrame frame2 = new JFrame();
-		JPanel panel2 = new JPanel();
-		frame2.setSize(600, 300);
-		frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		frame2.add(panel2);
-		frame2.setVisible(true);
-		panel2.setLayout(null);
-		button = new JButton("Confirm");
-		button.setBounds(240, 100 , 100, 35);
-		button.addActionListener(new SignUp());
-		panel2.add(button);
-
-		do {
-			password2Label = new JLabel("Please enter your password again to confirm:");
-			password2Label.setBounds(10, 20, 700, 15);
-			panel2.add(password2Label);
-			password2Text = new JPasswordField();
-			password2Text.setBounds(280, 20, 165, 25);
-			panel2.add(password2Text);
-			}while (!password2Text.equals(passwordText));
+		if (!password2.equals(password)) {
+			success.setText("Please enter again your password correctly");
+			
+		}else {
+			success.setText("You have signed up!");
 		
 		Account user = new Account(name, surname, password, phone, birthdate, gender, municipality);
 		
@@ -138,6 +136,6 @@ public class SignUp implements ActionListener{
 		
 		MainMenu User = new MainMenu(user);
 		User.printMenu();
-		
+	}
 	}
 }
