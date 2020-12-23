34032b3f824ea9b335e115a335899319768c1725
constantinazouni <72806587+constantinazouni@users.noreply.github.com>
2020-12-23 11:10:46 +0200

    UserStatePanel

--- src/UserStatePanel.java
+++ src/UserStatePanel.java
@@ -2,6 +2,10 @@ import java.awt.Color;
 
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class UserStatePanel extends JPanel {
 
@@ -13,10 +17,26 @@ public class UserStatePanel extends JPanel {
 		setBackground(new Color(176, 196, 222));
 		setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("user state panel");
-		lblNewLabel.setBounds(136, 119, 116, 14);
+		JLabel lblNewLabel = new JLabel("Have you been tested positive or negative for COVID-19?");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNewLabel.setBounds(10, 80, 400, 21);
 		add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Positive");
+		btnNewButton.setBackground(Color.LIGHT_GRAY);
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton.setBounds(99, 119, 89, 31);
+		add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Negative");
+		btnNewButton_1.setBackground(Color.LIGHT_GRAY);
+		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnNewButton_1.setBounds(227, 119, 97, 31);
+		add(btnNewButton_1);
 		setVisible(true);
 	}
-
 }
