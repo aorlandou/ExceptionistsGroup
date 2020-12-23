26adb6c42cf54a632b106cad790f0a439fa9cb7e
constantinazouni <72806587+constantinazouni@users.noreply.github.com>
2020-12-23 13:15:25 +0200

    UserState

--- src/UserStatePanel.java
+++ src/UserStatePanel.java
@@ -24,12 +24,22 @@ public class UserStatePanel extends JPanel {
 		add(lblNewLabel);
 		
 		JRadioButton btnNewButton = new JRadioButton("Positive");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				accountnow.userState("yes");
+			}
+		});
 		btnNewButton.setBackground(Color.LIGHT_GRAY);
 		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 12));
 		btnNewButton.setBounds(99, 119, 89, 31);
 		add(btnNewButton);
 		
 		JRadioButton btnNewButton_1 = new JRadioButton("Negative");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				accountnow.userState("no");
+			}
+		});
 		btnNewButton_1.setBackground(Color.LIGHT_GRAY);
 		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 12));
 		btnNewButton_1.setBounds(227, 119, 97, 31);
